c85c061c706db08ff9373c7717fd189479df5d52
MouliPvs <mouli.pvs@gmail.com>
2021-09-26 15:50:36 +0530

    Corrected Variable Name To products

--- product/views.py
+++ product/views.py
@@ -12,9 +12,9 @@ class LatestProductsList(APIView):
     """Get The Latest Products On The Front Page"""
     def get(self, request, format=None):
         #Gets First 4 Latest Product Objects From DataBase
-        producuts = Product.objects.all()[0 : 4]
+        products = Product.objects.all()[0 : 4]
 
         #converts products into serializer, many=True because we have more than one object
-        serializer = ProductSerializer(producuts, many=True)
+        serializer = ProductSerializer(products, many=True)
 
         return Response(serializer.data)
